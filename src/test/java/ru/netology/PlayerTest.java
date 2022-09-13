2af915b20d4c3bd53e1cfb705d0f6a38afde2c97
alexc <alexchb.makarenko@yandex.ru>
2022-09-13 21:37:10 +0300

    PlayerTests

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,6 +1,8 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
@@ -9,6 +11,7 @@ public class PlayerTest {
     public void shouldSumGenreIfOneGame() {
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
 
         Player player = new Player("Petya");
         player.installGame(game);
@@ -19,5 +22,116 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
 
-    // другие ваши тесты
+    @Test
+    public void shouldSumGenreIfTwoGame() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Баттл", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.play(game, 10);
+        player.play(game1, 10);
+
+        int expected = 20;
+        int actual = player.sumGenre(game.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSumGenreIfTwoGenre() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Баттл", "Гонки");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.play(game, 10);
+        player.play(game1, 10);
+
+        int expected = 0;
+        int actual = player.sumGenre(game.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mostTimePlayerByGenre() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Баттл", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.play(game, 10);
+        player.play(game1, 20);
+
+        Game expected = game1;
+        Game actual = player.mostPlayerByGenre("Аркады");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mostTimePlayerByNotGenre() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Баттл", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.installGame(game1);
+        player.play(game, 10);
+        player.play(game1, 20);
+
+        Game expected = null;
+        Game actual = player.mostPlayerByGenre("Гонки");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void installGame() {
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game1 = store.publishGame("Баттл", "Аркады");
+
+        Player player = new Player("Petya");
+        player.installGame(game);
+        player.play(game, 1);
+        player.installGame(game);
+
+        int expected = 1;
+        int actual = player.sumGenre(game.getGenre());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void negativeTime() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Баттл", "Аркады");
+
+        Player player = new Player("Petya");
+
+        player.installGame(game);
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            player.play(game, -1);
+        });
+    }
+
+    @Test
+    public void notInstalledGame() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Баттл", "Аркады");
+
+        Player player = new Player("Petya");
+
+
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            player.play(game, 3);
+        });
+    }
 }
